ceb97fe91a00dd3023ff9bc18c8cefa98c3e98d1
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-11-12 17:57:35 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,16 +3,28 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
+# class EmailAddressParser
+#
+#  attr_reader :emails
+#
+#  def initialized(emails)
+#    @emails = emails
+#  end
+#
+#  def parse
+#
+#  end
+#
+# end
+
 class EmailAddressParser
+  attr_reader :emails
 
- attr_reader :emails
+  def initialize(emails)
+    @emails = emails
+  end
 
- def initialized(emails)
-   @emails = emails
- end
-
- def parse
-
- end
-
+  def parse
+    emails.split(/, | /).uniq
+  end
 end
